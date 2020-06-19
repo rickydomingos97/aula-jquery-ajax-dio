ab717083ba6c080f3557534a798373889e7e9f85
iricardo97 <ricardodomingos97@gmail.com>
2020-06-19 20:08:29 -0300

    creation of tables and insert cep ajax on main.js

--- js/main.js
+++ js/main.js
@@ -9,8 +9,12 @@ function consultaCep() {
             console.log(response);
             $("#logradouro").html(response.logradouro);
             $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("CEP " + response.cep);
+
+            //Outra forma de fazer as chamadas ajax:
             //document.getElementById("logradouro").innerHTML = response.logradouro;
-            
             //document.getElementById("bairro").innerHTML = response.bairro;
             //document.getElementById("localidade").innerHTML = response.localidade;
             //document.getElementById("uf").innerHTML = response.uf;
